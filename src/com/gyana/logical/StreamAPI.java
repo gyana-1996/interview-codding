2bcc1480f2bc22bcfa5fdb49ca91a87a8b498355
AzureAD\GyanaBarik <gyana.barik@waisl.in>
2025-03-12 12:42:08 +0530

    find duplicates of number added

--- src/com/gyana/logical/StreamAPI.java
+++ src/com/gyana/logical/StreamAPI.java
@@ -1,6 +1,10 @@
 package com.gyana.logical;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
@@ -12,8 +16,16 @@ public class StreamAPI {
 
 		Map<Character, Long> mapObs = str.chars().mapToObj(c -> (char) c)
 				.collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
-		
+
 		mapObs.entrySet().forEach(x -> System.out.println(x));
+
+		// Find Duplicates from ArrayList
+
+		List<Integer> listNo = Arrays.asList(5, 10, 4, 5, 22, 35, 99, 10);
+
+		Set<Integer> uniqueList = new HashSet<>();
+
+		listNo.stream().filter(x -> !uniqueList.add(x)).collect(Collectors.toSet()).forEach(System.out::println);
 
 	}
 
